0e7c2720d339924f5e5dd7d01b8c62363a76b69f
권민정 <22alswjd@naver.com>
2021-07-24 00:00:10 +0900

    feature <within24hours> is included

--- src/main/java/com/sparta/week03/controller/MemoController.java
+++ src/main/java/com/sparta/week03/controller/MemoController.java
@@ -7,6 +7,7 @@ import com.sparta.week03.service.MemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -24,7 +25,9 @@ public class MemoController {
 
     @GetMapping("/api/memos")
     public List<Memo> readMemo() {
-        return memoRepository.findAllByOrderByModifiedAtDesc();
+        LocalDateTime start = LocalDateTime.now().minusDays(1);
+        LocalDateTime end = LocalDateTime.now();
+        return memoRepository.findAllBymodifiedAtBetweenOrderByModifiedAtDesc(start, end);
     }
 
     @PutMapping("/api/memos/{id}")
